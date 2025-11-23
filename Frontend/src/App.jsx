7c394eb8577164c7546fc31745cecc1a0d4bc558
Nisha <nishaamishra10@gmail.com>
2025-11-24 03:56:48 +0530

    added new files

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,28 @@
-import { BrowserRouter , Route , Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
 import { Toaster } from "sonner";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import CollectionPage from "./pages/CollectionPage";
 
 const App = () => {
   return (
     <BrowserRouter
-     future={{ v7_startTransition: true, v7_relativeSplaPath: true}}>
-    <Toaster position ="top-right" />
-    <Routes>
-       <Route path="/" element={<UserLayout />} >
-       <Route index element ={<Home />} />
-       </Route>
-       <Route>{/* Admin Layout */}</Route>
-    </Routes>
+      future={{ v7_startTransition: true, v7_relativeSplaPath: true }}
+    >
+      <Toaster position="top-right" />
+      <Routes>
+        <Route path="/" element={<UserLayout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="collections/:collection" element={<CollectionPage />} />
+        </Route>
+        <Route>{/* Admin Layout */}</Route>
+      </Routes>
     </BrowserRouter>
   );
 };
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,37 +1,69 @@
-import React from 'react'
-import Hero from '../components/Layout/Hero'
-import GenderCollectionSection from '../components/Product/GenderCollectionSection'
-import NewArrivals from '../components/Product/NewArrivals'
-import ProductDetails from '../components/Product/ProductDetails'
-import ProductGrid from '../components/Product/ProductGrid'
+import React from "react";
+import Hero from "../components/Layout/Hero";
+import GenderCollectionSection from "../components/Product/GenderCollectionSection";
+import NewArrivals from "../components/Product/NewArrivals";
+import ProductDetails from "../components/Product/ProductDetails";
+import ProductGrid from "../components/Product/ProductGrid";
+import FeaturesCollections from "../components/Product/FeaturesCollections";
+import FeaturedSection from "../components/Product/FeaturedSection";
 
 const placeholderProducts = [
-   {
-          _id: 4,
-          name: "Product 1" ,
-          price: 100,
-          images: [{url : "https://picsum.dev/300/200?random=6 "}],
-      },
-      {
-          _id: 5,
-          name: "Product 1" ,
-          price: 100,
-          images: [{url : "https://picsum.dev/300/200?random=7 "}],
-      },
-      {
-          _id: 6,
-          name: "Product 2" ,
-          price: 100,
-          images: [{url : "https://picsum.dev/300/200?random=8"}],
-      },
-      {
-          _id: 7,
-          name: "Product 3" ,
-          price: 100,
-          images: [{url : "https://picsum.dev/300/200?random=9"}],
-      },
-     
-]
+  {
+    _id: 1,
+    name: "Product 1",
+    price: 100,
+    images: [{ url: "https://picsum.dev/300/200?random=1" }],
+  },
+
+  {
+    _id: 2,
+    name: "Product 2",
+    price: 100,
+    images: [{ url: "https://picsum.dev/300/200?random=2" }],
+  },
+
+  {
+    _id: 3,
+    name: "Product 3",
+    price: 100,
+    images: [{ url: "https://picsum.dev/300/200?random=3" }],
+  },
+
+  {
+    _id: 4,
+    name: "Product 4",
+    price: 100,
+    images: [{ url: "https://picsum.dev/300/200?random=4" }],
+  },
+
+  {
+    _id: 5,
+    name: "Product 5",
+    price: 100,
+    images: [{ url: "https://picsum.dev/300/200?random=5" }],
+  },
+
+  {
+    _id: 6,
+    name: "Product 6",
+    price: 100,
+    images: [{ url: "https://picsum.dev/300/200?random=6" }],
+  },
+
+  {
+    _id: 7,
+    name: "Product 7",
+    price: 100,
+    images: [{ url: "https://picsum.dev/300/200?random=7" }],
+  },
+
+  {
+    _id: 8,
+    name: "Product 8",
+    price: 100,
+    images: [{ url: "https://picsum.dev/300/200?random=8" }],
+  },
+];
 
 const Home = () => {
   return (
@@ -50,8 +82,12 @@ const Home = () => {
         </h2>
         <ProductGrid products={placeholderProducts} />
       </div>
-    </div>
-  )
-}
 
-export default Home
+      <FeaturesCollections />
+
+      <FeaturedSection />
+    </div>
+  );
+};
+
+export default Home;
